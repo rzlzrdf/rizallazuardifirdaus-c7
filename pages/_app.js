ba5719210411f0fa1a15b13417745128516aab33
rzlzrdf <rizallazu@student.ub.ac.id>
2022-05-24 13:22:47 +0700

    @sun #add routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
 
-  <ul></ul>
-  return <Component {...pageProps} />
+  
+  return (
+    <>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/input">Input</Link></li>
+        <li><Link href="/chart">Chart</Link></li>
+        <li><Link href="/download">Download</Link></li>
+      </ul>
+      <Component {...pageProps} />
+    </>
+  
+  )
 }
 
 export default MyApp
